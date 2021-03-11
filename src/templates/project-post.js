f0f20da3725b8d16134527e491795ac5b944a650
semyers189 <semyers@fullsail.edu>
2021-03-11 10:56:02 -0500

    added generated project pages

--- src/templates/project-post.js
+++ src/templates/project-post.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { graphql } from 'gatsby'
 
-const ProjectPost = () => {
+const ProjectPost = ({ data }) => {
+  const post = data.markdownRemark
   return (
     <main>
-      project post
+      {post.frontmatter.title}
     </main>
   )
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html,
+      frontmatter {
+        title
+      }
+    }
+  }
+`
 
 export default ProjectPost
