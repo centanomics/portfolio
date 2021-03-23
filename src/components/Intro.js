019732fa4bfb1e996ba6d122f33bf78ff3c7e5c0
semyers189 <semyers@fullsail.edu>
2021-03-23 14:03:10 -0400

    added styles for the description text

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,6 +1,7 @@
 // imports
 
 import * as React from "react"
+import { Link } from 'gatsby'
 
 // markup
 
@@ -9,17 +10,17 @@ const Footer = () => {
     <div>
       <main className="frame intro">
         <section>
-          <div>
+          <div className="description" >
             <h2>Hello, world!</h2>
             <h1>I'm <span>Shannon</span></h1>
             <p>I design and develop Full Stack Applications</p>
             <nav>
-              <a>View Projects</a>
+              <a href="#projects">View Projects</a>
               <span> or </span>
-              <a>Read About Me</a>
+              <Link to="/about"> Read About Me</Link>
             </nav>
           </div>
-          <button>
+          <button className="image" >
             <img src="/me.png" />
           </button>
         </section>
