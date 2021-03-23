50380e206e8e95b94d259d206439070992f46de8
semyers189 <semyers@fullsail.edu>
2021-03-23 17:44:47 -0400

    started on adding the project tiles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,50 @@
 // imports
 
 import * as React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 // markup
 
 const Projects = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        allMarkdownRemark(sort: { fields: [frontmatter___index], order: ASC }) {
+          edges {
+            node {
+              frontmatter {
+                image
+                startDate(formatString: "MMMM YYYY")
+                title
+                description
+                repo
+                dark
+              }
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
   return (
-    <div>
-      <section className="frame">
-          Projects
-      </section>
-    </div>
+    <section className="frame" id="Projects">
+      {data.allMarkdownRemark.edges.map(({ node }, index) => {
+        console.log(node.fields.slug)
+        return (
+          <article key={index} className="project">
+            <Link to={node.fields.slug}>
+              <div style={{ background: `url(${node.frontmatter.image})` }}>
+                hi
+              </div>
+            </Link>
+          </article>
+        )
+      })}
+    </section>
   )
 }
 
