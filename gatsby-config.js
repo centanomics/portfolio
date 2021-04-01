2147678de224ac1c10504e981f4b4b3b437a2bd4
semyers189 <semyers@fullsail.edu>
2021-04-01 18:06:00 -0400

    added relevant gatsby plugins for a pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,20 @@ module.exports = {
         path: `${__dirname}/src/projects/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Centanomics Portfolio`,
+        short_name: `Cent's Portfolio`,
+        start_url: `/`,
+        background_color: `#29339b`,
+        theme_color: `#29339b`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.svg`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 };
