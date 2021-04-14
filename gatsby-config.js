c7867d549a3f5d75102e8ac014b90101e4946673
semyers189 <semyers@fullsail.edu>
2021-04-14 18:35:53 -0400

    removed unnecessary comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,10 +26,8 @@ module.exports = {
         start_url: `/`,
         background_color: `#29339b`,
         theme_color: `#29339b`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/images/icon.svg`, // This path is relative to the root of the site.
+        icon: `src/images/icon.svg`,
       },
     },
     `gatsby-plugin-offline`,
